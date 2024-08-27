07e6b7ed416709e5dc47949b41ee14866fa43558
m4rcosmatos <marcosmattos026@gmail.com>
2024-08-27 15:39:32 -0300

    configurando router

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,14 @@
-import Title from "./components/Title";
-import Counter from "./components/Counter";
+import { Outlet } from "react-router-dom";
 
 import NewYear from "./assets/newyear.jpg";
-
-import useCountdown from "./hooks/useCountdown";
 
 import "./App.css";
 
 function App() {
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2025 00:00:00");
   return (
     <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
       <div className="container">
-        <Title title="Contagem regressiva para 2025" />
-        <div className="countdown-container">
-          <Counter title="Dias" number={day} />
-          <Counter title="Horas" number={hour} />
-          <Counter title="Minutos" number={minute} />
-          <Counter title="Segundos" number={second} />
-        </div>
+        <Outlet />
       </div>
     </div>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+// pages
+
+import Home from "./routes/Home.jsx";
+import Countdown from "./routes/Countdown.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/countdown",
+        element: <Countdown />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
